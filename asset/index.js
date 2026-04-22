c618018e025983fb4a3e13c9a0ee26d2f9a77a3b
idil-abdi <edilabdiaziz@gmail.com>
2026-04-22 13:42:12 +0100

    first commit

---
+++ asset/index.js
@@ -0,0 +1,124 @@
+function hamburgerBar () {
+    const x =document.getElementById('myLink');
+    if (x.style.display === 'block') {
+        x.style.display ='none';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+// form validation
+const nameError = document.getElementById('name-error')
+const emailError = document.getElementById('email-error')
+const messageError = document.getElementById('message-error')
+const submitError = document.getElementById('submit-error')
+
+function validateName() {
+  const name = document.getElementById('contact-name').value;
+  if(name.length == 0){
+    nameError.innerHTML = 'Enter full name.';
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = 'Write full name';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+    const email = document.getElementById('contact-email').value;
+    if(email.length == 0){
+        emailError.innerHTML = 'Enter email address.';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
+    }
+      emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+      return true;
+}
+
+function validateMessage() {
+    const message = document.getElementById('contact-message').value;
+    const required = 30;
+    const left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' more characters required';
+    return false;        
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage() ) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000)
+        return false;
+    }
+}
+
+// portfolio filter
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+// animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden')
+hiddenElement.forEach((el) => observer.observe(el))
